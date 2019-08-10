c1e1de587b651194e0ff722269a554c1db7599fe
Eric Flatt <scadstartup2019@gmail.com>
2019-08-09 21:11:56 -0400

    Initial Commit

---
+++ SwiftUi_Demo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUi_Demo
+//
+//  Created by Eric Flatt on 8/9/19.
+//  Copyright © 2019 Eric Flatt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
