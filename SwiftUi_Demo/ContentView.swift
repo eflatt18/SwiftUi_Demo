bdb14b34272b22c954699f59c956a884d432980a
Eric Flatt <eflatt18@gmail.com>
2019-08-09 21:47:47 -0400

    blue color change

--- SwiftUi_Demo/ContentView.swift
+++ SwiftUi_Demo/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         Text("Hello world")
             .font(.title)
-            .foregroundColor(.green) 
+            .foregroundColor(.blue)
             
         
     }
